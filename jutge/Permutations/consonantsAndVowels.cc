a8f3bc63e0968403b5ae8fec2aca91be09a370c9
Cami <cameliabrumar@gmail.com>
2017-10-17 17:14:52 +0200

    consonantsAndVowels --> semàfor verd

--- jutge/Permutations/consonantsAndVowels.cc
+++ jutge/Permutations/consonantsAndVowels.cc
@@ -3,44 +3,46 @@
 #include <algorithm>
 using namespace std;
 
-// el vector perm és la permutació en si, o sigui, el que sortirà per la pantalla.
-// el vector triat ens diu si el numero corresponent la posició j ha sortit en el vector perm o no.
-// triat[j] = 1 si el numero j ha sortit al vector perm, triat[j] = 0 altrament. 
-
 using VE = vector<int>;
-VE triat, perm;
+using VEE = vector<VE>;
+using VC = vector<char>;
+using VCC = vector<VC>;
+
+VE permut;
+VEE usat;
+VCC lletres;
 int n;
 
 void escriu() {
-  cout << '(';
-  bool first = true;
-  for (int i = 0; i < n; ++i) {
-    if (first) {
-      cout << perm[i] + 1;
-      first = false;
-    }
-    else cout << ',' << perm[i] + 1;
-  }
-  cout << ')' << endl;
+  for (int i = 0; i < 2*n; ++i)
+    cout << lletres[i%2][permut[i]];
+  cout << endl;
 }
 
 void f(int i) {
-  if (i == n) return escriu();
-
+  if (i == 2*n) return escriu();
   for (int j = 0; j < n; ++j) {
-    if (!triat[j]) {
-      perm[i] = j;
-      triat[j] = 1;
-      f(i + 1);
-      triat[j] = 0;
+    if (!usat[i%2][j]) {
+        permut[i] = j;
+        usat[i%2][j] = 1;
+        f(i+1);
+        usat[i%2][j] = 0;
     }
   }
-
 }
 
 
 int main() {
   cin >> n;
-  triat = perm = VE(n);
+  lletres = VCC(2, VC(n));
+  usat = VEE(2, VE(n, 0));
+  for(int i = 0; i < 2; ++i) {
+    //cout << "i = " << i << endl;
+    for(int j = 0; j < n; ++j) {
+      //cout << "       j = " << j << endl;
+      cin >> lletres[i][j];
+    }
+  }
+  permut = VE(2*n);
   f(0);
 }
