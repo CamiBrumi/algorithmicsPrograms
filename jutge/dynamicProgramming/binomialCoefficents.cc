10d717be4f8218be2b7f221abcf3fe72fe60888f
Cami <cameliabrumar@gmail.com>
2017-10-24 08:18:17 +0200

    he començat el de binomial coefficients

---
+++ jutge/dynamicProgramming/binomialCoefficents.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using ll = long long;
+using VE = vector<int>;
+
+int const N = 30;
+int const K = 30;
+VE v;
+
+int main() {
+  v = VE(N, VE(K, -1))
+  int n, m;
+  while (cin >> n >> k) cout << bc(n, k);
+}
