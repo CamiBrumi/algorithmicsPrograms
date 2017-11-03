97c6fbd39ec6c455e788720c177ccbb9989211b9
Cami <cameliabrumar@gmail.com>
2017-11-03 21:54:09 +0100

    He entrat el problema escapeFromTheMaze al jutge!

--- jutge/dynamicProgramming/scapeFromTheMaze.cc
+++ jutge/dynamicProgramming/scapeFromTheMaze.cc
@@ -3,18 +3,22 @@
 using namespace std;
 using VC = vector<char>;
 using VCC = vector<VC>;
+using ll = long long;
+using VE = vector<ll>;
+using VEE = vector<VE>;
+VEE V;
 int n, m;
 VCC lab;
 long INF = 1e6;
 
-long long paths(int i, int j) {
-  if (i == 0 and j == 0) return 1;
-  if (lab[i][j] == '.') {
-    if (i == 0) return paths(i, j - 1);
-    if (j == 0) return paths(i - 1, j);
-    return paths(i-1, j) + paths(i, j-1);
-  }
-  return 0;
+ll paths(int i, int j) {
+  if (V[i][j] != -1) return V[i][j];
+  if (lab[i][j] == 'X') return 0;
+  int res = 0;
+  if (i) res += paths(i-1, j);
+  if (j) res += paths(i, j-1);
+  if (res > INF) res = INF;
+  return (V[i][j] = res);
 }
 
 int main() {
@@ -25,8 +29,11 @@ int main() {
       for (int j = 0; j < m; ++j) {
         cin >> lab[i][j];
       }
-      long long correctPaths = paths(n-1, m-1);
-      else cout << correctPaths << endl;
-      cin >> n >> m;
+    V = VEE(n, VE(m, -1));
+    V[0][0] = 1;
+    ll correctPaths = paths(n-1, m-1);
+    if (correctPaths >= INF) cout << "!!!" << endl;
+    else cout << correctPaths << endl;
+    cin >> n >> m;
   }
 }
